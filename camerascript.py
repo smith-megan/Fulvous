03a57e334544d60a3a204d0670289d3e548e3c10
Megan <smith.megan.design@gmail.com>
2024-06-10 14:12:49 -0700

    added sleep and directory placement functionality

--- camerascript.py
+++ camerascript.py
@@ -1,13 +1,24 @@
 import cv2
+import os
+from time import sleep
 
 cam= cv2.VideoCapture(0)
+directory = r'C:\Users\MegaN\Documents\Fulvous\Fulvous'
 
+# show test shot
 while True:
     ret, image = cam.read()
     cv2.imshow('Imagetest' ,image)
     k = cv2.waitKey(1)
     if k != -1:
         break
-cv2.imwrite('/home/megan/pi/testimage.jpg', image)
+os.chdir(directory)
+
+# start timelapse
+for i in range(3):
+    ret, image = cam.read()
+    cv2.imwrite('imagetest{0:04d}.jpg'.format(i), image)
+    sleep(10)
+
 cam.release()
 cv2.destroyAllWindows()
